cc162dad00341cd2680efdd14254092359316f23
RestiAdelia <adeliaresti101@gmail.com>
2025-12-30 14:38:52 +0700

    Finally code

--- resources/views/absen/index.blade.php
+++ resources/views/absen/index.blade.php
@@ -141,17 +141,19 @@
                                             d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                                     </svg>
                                 </a>
-                                <a href="{{ route('absen.siswa.show', ['siswa' => $s->id, 'start_date' => $start->toDateString()]) }}"
+                                  @if (auth()->user()->role !== 'orangtua')
+                                <a href="{{ route('absen.siswa.edit', ['siswa' => $s->id, 'week' => $start->toDateString()]) }}"
                                     class="p-1.5 text-gray-400 hover:text-indigo-600 hover:bg-indigo-50 rounded-lg transition-all duration-200 group"
                                     title="Lihat Detail">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none"
-                                        viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                                    </svg>
+                                    <button title="Edit Data" class="text-yellow-600 hover:text-yellow-800">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none"
+                                            viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M11 5h2M12 4v2m7.071 1.071a5 5 0 010 7.071L9 21l-4 1 1-4 10.071-10.071a5 5 0 017.071 0z" />
+                                        </svg>
+                                    </button>
                                 </a>
+                                @endif
                                 @if (Auth::user()->role !== 'orangtua')
                                     <form action="{{ route('absen.destroyWeekly', $s->id) }}" method="POST"
                                         class="delete-form">
