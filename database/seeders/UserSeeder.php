d1b95dd0c24fcd9202b014737ef8b2c8aeb1a1f0
RestiAdelia <adeliaresti101@gmail.com>
2026-01-02 17:31:49 +0700

    data user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,27 +3,33 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        User::create([
-            'name' => 'Operator Sekolah',
-            'email' => 'admin@gmail.com',
-            'password' => Hash::make('admin123'),
-        ]);
+        $users = [
+            [
+                'name' => 'Operator Sekolah',
+                'email' => 'sdnsiska13@gmail.com',
+                'password' => Hash::make('admin123'),
+                'role' => 'admin',
+            ],
+            [
+                'name' => 'Operator Sekolah 2',
+                'email' => 'sdnsiska13@gmail.com',
+                'password' => Hash::make('admin123'),
+                'role' => 'admin',
+            ],
+        ];
 
-        User::create([
-            'name' => 'Operator Sekolah 2',
-            'email' => 'sdnsiska13@gmail.com',
-            'password' => Hash::make('admin123'),
-        ]);
+        foreach ($users as $user) {
+            User::updateOrCreate(
+                ['email' => $user['email']], 
+                $user
+            );
+        }
     }
 }
